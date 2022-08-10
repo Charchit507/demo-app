b84a6a1e8834cfdba7b48750b2337e8ab2201a0b
Charchit507 <charchitkhandelwal575@gmail.com>
2022-08-10 14:43:00 +0530

    comments controller file modified

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,25 @@
 class CommentsController < ApplicationController
-  before_action :comment_create, only: [:create]
-  before_action :comment_destroy, only: [:destroy]
+  before_action :find_article, only: [:create, :destroy]
+
   def create
+    @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
   def destroy
+    @comment = @article.comments.find(params[:id])
+
     @comment.destroy
     redirect_to article_path(@article)
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:commenter, :body)
   end
 
-  def comment_destroy
+  def find_article
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-  end
-
-  def comment_create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
   end
 end
