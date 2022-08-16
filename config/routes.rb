21a9dce668062a5afab896cbbd48d1dd7093459a
Charchit507 <charchitkhandelwal575@gmail.com>
2022-08-16 12:43:16 +0530

    modify routes files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    passwords: 'users/passwords'
-  }
+  devise_for :users
 
   resources :articles do
     resources :comments
