f3869f7e97992b57d63f4fdfbfdf4aadb6b5ef40
Charchit507 <charchitkhandelwal575@gmail.com>
2022-08-23 11:24:16 +0530

    full_name method created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
     @comment = @article.comments.find(params[:id])
 
     @comment.destroy
-    redirect_to article_path(@article)
+    redirect_to article_path(@article), notice: "Comment Destroyed Successfully"
   end
 
   private
